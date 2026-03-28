39ac44c1e86793fcf091f4da8b4359f66c13b3f5
anmolmishra99 <mishraanmol258@gmail.com>
2026-03-28 17:16:55 +0530

    changes -2

---
+++ app/landing-page-2/layout.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import StyledJsxRegistry from "@/components/landing-page-2/registry";
+
+export default function LandingPage2Layout({ children }) {
+  return (
+    <StyledJsxRegistry>
+      {children}
+    </StyledJsxRegistry>
+  );
+}
